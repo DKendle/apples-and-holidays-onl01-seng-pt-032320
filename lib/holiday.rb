9bf720e488cbf70b4af73a845eb4d159c50f8f1a
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 22:58:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,6 +34,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
@@ -42,7 +43,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  " + holiday + " " + supplies
     end
   end
-end
 end
 
 def all_holidays_with_bbq(holiday_hash)
