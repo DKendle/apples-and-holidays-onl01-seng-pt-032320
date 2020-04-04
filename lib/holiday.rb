53fcb69b401002f56d2dcb1fb80b6fcd98512fe1
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 22:32:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,8 +22,8 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
  
- holiday_hash[:holiday_name] = {
-   []
+ holiday_hash[:season] = {
+   :holiday_name
  } # code here
   # remember to return the updated hash
 
